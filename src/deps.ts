380733b7b6b1bf74a75aa80e9d12034e4478af09
Fatih Kadir Akın <fatihkadirakin@gmail.com>
2023-09-29 12:18:18 +0300

    Update deps.ts

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export type { MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.3.1/server.ts";
+export type { MiddlewareHandlerContext } from "https://deno.land/x/fresh@1.4.3/server.ts";
 export {
   type Cookie,
   deleteCookie,
